fa06ed39681700a083cf3e8eace9c2e9e1e1a325
SATDI09 <sarthdivate4@gmail.com>
2024-09-07 17:56:29 +0530

    Modify small in app 1

--- src/App.js
+++ src/App.js
@@ -1,56 +1,69 @@
 import { useState } from "react";
 
-export default function App()
-{
-
-    function handleAdd()
-    {
-      setCount(count + 1);
-    }
-
-    function handleSubtract()
-    {
-      setCount(count -1);
-    }
-
+export default function App() {
   const [count, setCount] = useState(0);
   const [tasks, setTasks] = useState([
-    {id: 1, name: "Task 1", completed: true},
-    {id: 2, name: "Task 2", completed: false},
-    {id: 3, name: "Task 3", completed: false},
-  ])
+    { id: 1, name: "Task 1", completed: true },
+    { id: 2, name: "Task 2", completed: false },
+    { id: 3, name: "Task 3", completed: false },
+  ]);
+
+  const onDelete = (i) => {
+    const del = [...tasks];
+    del.splice(i, 1);
+    setTasks(del);
+  };
+
   return (
     <div className="App">
       <div className="box">
-      <h2> Counter </h2>
+        <h2> Counter </h2>
         <p>{count}</p>
-        <button  onClick={handleAdd} className="add"> ADD </button>
-        <button onClick={handleSubtract} className="sub"> SUBTRACT </button>
+        <button onClick={() => setCount(count + 1)} className="add">
+          {" "}
+          ADD{" "}
+        </button>
+        <button onClick={() => setCount(count - 1)} className="sub">
+          {" "}
+          SUBTRACT{" "}
+        </button>
       </div>
-    <hr/>
-    <div className="box">
+      <hr />
+      <div className="box">
         <h2> Task List </h2>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            setTasks((prev) => [
+              ...prev,
+              {
+                id: prev[tasks.length - 1].id + 1,
+                name: e.target[0].value,
+                completed: "Yes",
+              },
+            ]);
+          }}
+        >
+          <input type="text"></input>
+          <button type="submit">Add Record</button>
+        </form>
         <ul>
-          
-            <li>
-                 <span>Id</span>
-                 <span>Name</span>
-                 <span>Status</span>
-                 <span>Action</span>
-              </li>
-          {
-            tasks.map((task) => (
-              <li key = {task.id}>
-                 <span>{task.id}</span>
-                 <span>{task.name}</span>
-                 <span>{task.completed ? 'yes' : 'no'}</span>
-                 <button>delete</button>
-              </li>
-            ))
-          }
+          <li>
+            <span>Id</span>
+            <span>Name</span>
+            <span>Status</span>
+            <span>Action</span>
+          </li>
+          {tasks.map((task, i) => (
+            <li key={task.id}>
+              <span>{task.id}</span>
+              <span>{task.name}</span>
+              <span>{task.completed ? "yes" : "no"}</span>
+              <button onClick={() => onDelete(i)}>Delete</button>
+            </li>
+          ))}
         </ul>
+      </div>
     </div>
-
-    </div>
-  )
+  );
 }
