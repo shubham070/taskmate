a1d7593e2e25c48408b30e7a04c1164a079b40a4
shubham070 <shubhamg070@gmail.com>
2024-09-07 16:29:45 +0530

    1. 1st attempt to create a function component. 2. add counter on the UI 3. Why  use state and indr to use State

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+export default function App()
+{
+
+    function handleAdd()
+    {
+      setCount(count + 1);
+    }
+
+    function handleSubtract()
+    {
+      setCount(count -1);
+    }
+
+  const  [count, setCount] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="box">
+        <p>{count}</p>
+        <button  onClick={handleAdd} className="add"> ADD </button>
+        <button onClick={handleSubtract} className="sub"> SUBTRACT </button>
+      </div>
     </div>
-  );
+  )
 }
-
-export default App;
