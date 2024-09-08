c853715dd42c476447cbe57ba45bb84cb1a7e200
shubham070 <shubhamg070@gmail.com>
2024-09-08 19:10:57 +0530

    add exception handling via try catch

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,21 +1,29 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
-export default function useFetch(url)
-{
-    const [data, setData] = useState(null);
-    const [loading, setLoading] = useState(false);
-
-    useEffect(()=>
-    {
-        const fetchData = async () => {
-            setLoading(true);
-            const response = await fetch(url);
-            const result = await response.json();
-            setLoading(false);
-            console.log('data +'+ result)
-            setData(result);
+export default function useFetch(url) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setLoading(true);
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error(response.statusText);
         }
-        fetchData();
-    },[url])
-    return {data, loading }
+        const result = await response.json();
+        setLoading(false);
+        console.log("data +" + result);
+        setData(result);
+        setError("");  
+        
+      } catch (error) {
+        console.log(error);
+        setError(error.message);
+      }
+    };
+    fetchData();
+  }, [url]);
+  return { data, loading, error };
 }
