84573d1acb4ea47286ca446da3d9fb7bb40b7f4e
shubham070 <shubhamg070@gmail.com>
2024-09-08 15:41:21 +0530

    use of useCallback with use effect

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,21 +1,38 @@
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 export default function Products() {
-  const getProducts = () => {
-    fetch("http://localhost:8000/products")
+    const [url, setUrl] = useState("http://localhost:8000/products")
+    
+    const getFilteredProducts = () =>
+    {
+        setUrl("http://localhost:8000/products?Price_lt=200")
+    }
+
+    const getAllProducts = () =>
+        {
+            setUrl("http://localhost:8000/products")
+        }
+    
+    const getProducts = useCallback(async () => {
+    await fetch(url)
       .then((resp) => resp.json())
       .then((data) => setProducts(data));
-  };
+  },[url]);
 
   useEffect(() => {
     getProducts();
-  }, []);
+//    setCount(count+1)
+  },[getProducts]);
 
   const [products, setProducts] = useState([]);
 
   return (
     <div className="box">
       <ul>
+       <li>
+            <button onClick={getAllProducts}>All Products </button>
+            <button onClick={getFilteredProducts}>Prods below Rs-200 </button>
+       </li>
         {products.map((x) => (
           <li><span>{x.Id}</span> <span>{x.Name} </span> </li>
         ))}
